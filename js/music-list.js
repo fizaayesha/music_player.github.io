0aabb2983686e2048d94cbed18f8d55fa8b6653b
Ayesha <fizaayesha696@gmail.com>
2022-01-30 23:05:49 +1200

    Music Player
    Music Player

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "Señorita",
+    artist: "Camila Cabello, Shawn Mendes",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Arcade",
+    artist: "Duncan Laurence",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Let Me Down Slowly",
+    artist: "Alec Benjamin",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Ae Dil Hai Mushkil",
+    artist: "Arijit Singh",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Tere Sang Yaara",
+    artist: "Atif Aslam",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Safari ",
+    artist: "Serena",
+    img: "music-6",
+    src: "music-6"
+  },
+
+];
